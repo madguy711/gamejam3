6d9347f905c6c437b560b7002ea58c321741100e
madguy711 <44320778+madguy711@users.noreply.github.com>
2023-04-04 12:56:25 +0800

    more pushes

---
+++ gamejam3/Assets/Scripts/ImpulseForceTest.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpulseForceTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float forceMultiplier = 25f; // the force applied to the player
+
+    private Rigidbody2D rb;
+
+    private bool isGrounded; // if player is touching the ground
+
+    private float shotsFired; // track the number of shots fired
+
+    private float lastShotTime; // for the reload time 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // check if left mouse button is clicked
+        {
+            if ((isGrounded && (shotsFired <= 1)) && (Time.time - lastShotTime > 1f)) // check if the player is touching the ground and hasn't fired more than two shots, wait one second before being able to fire after hitting ground
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // get the mouse position in world space
+                Vector2 direction = (mousePosition - transform.position).normalized; // get the direction of the click
+                direction.Normalize();
+
+                rb.AddForce(-direction * forceMultiplier, ForceMode2D.Impulse); // apply the impulse force in the opposite direction of the click
+                shotsFired++;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // apply the equations for 2D directional kinematic motion
+        Vector2 velocity = rb.velocity;
+        float vx = velocity.x;
+        float vy = velocity.y;
+
+        float time = Time.fixedDeltaTime; // get the time since the last fixed update
+        float gravity = Physics2D.gravity.y;
+
+        vx = vx; // horizontal velocity remains constant
+        vy = vy + gravity * time; // vertical velocity changes due to gravity
+
+        rb.velocity = new Vector2(vx, vy);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) // method for when the player touches the floor
+    {
+        if ((collision.gameObject.CompareTag("Floor")) && (shotsFired >= 2))
+        {
+            isGrounded = true;
+            rb.velocity = Vector2.zero; // reset velocity when landing on floor
+            shotsFired = 0f; // reset shots back to zero 
+            lastShotTime = Time.time; // make the player wait before shooting again
+        }
+
+        else // If player only fires one shot then lands then they instantly fire another before having to reload 
+        {
+            isGrounded = true;
+            rb.velocity = Vector2.zero; // reset velocity when landing on floor
+        }
+    }
+}
---
+++ gamejam3/Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    [SerializeField] private Rigidbody2D rb;  // Reference to the Rigidbody2D component
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if ((IsGrounded()) && (Input.GetKey(KeyCode.D))) // Prevent player from moving in air
+        {
+            MovePlayer(1f);
+        }
+        else if ((IsGrounded()) && (Input.GetKey(KeyCode.A)))
+        {
+            MovePlayer(-1f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+
+    private void MovePlayer(float direction)
+    {
+        float verticalVelocity = rb.velocity.y; // store the current vertical velocity of the player
+        rb.velocity = new Vector2(direction * moveSpeed, verticalVelocity); // only modify the horizontal velocity
+    }
+}
+
+
+
+
+
