4078b3793dbbdb76ee3aca4f898524fa20f92bf0
madguy711 <44320778+madguy711@users.noreply.github.com>
2023-04-05 13:10:26 +0800

    cameraswap works now

--- gamejam3/Assets/Scripts/CameraSwap.cs
+++ gamejam3/Assets/Scripts/CameraSwap.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class CameraSwap : MonoBehaviour
 {
-    public Camera cam1;
-    public Camera cam2;
-
-
-
     
+    public GameObject player;
+    public GameObject cam1;
+    public GameObject cam2;
+
+
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam1.SetActive(true);
+        cam2.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.transform.position.y >= 14f)
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+        }
+
+        else 
+        {
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+        }
     }
 }
