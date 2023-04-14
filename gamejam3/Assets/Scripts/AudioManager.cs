b35e930a0d690fc02468a10b83b428c85112e6aa
Butterx4 <100896088+Butterx4@users.noreply.github.com>
2023-04-14 09:20:26 +0800

    music

--- gamejam3/Assets/Scripts/AudioManager.cs
+++ gamejam3/Assets/Scripts/AudioManager.cs
@@ -17,6 +17,11 @@ public class AudioManager : MonoBehaviour
 
         }
     }
+
+    void Start()
+    {
+        Play("Music");
+    }
     public void Play (string name){
        Sound s = Array.Find(sounds, sound => sound.name == name);
        s.source.Play();
